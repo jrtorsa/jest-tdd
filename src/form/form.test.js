ee6fd675504fba2230bd2bb5ca087aef8016f538
Raul T <jr.torsa@gmail.com>
2020-12-14 23:40:39 -0700

    first commit

---
+++ src/form/form.test.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import {screen, render} from '@testing-library/react'
+import Form from './form'
+
+describe('when the form is mounted', () => {
+  it('there must be a create product form page', () => {
+    render(<Form />)
+    //     expect(screen.queryByText(/create product/i)).toBeInTheDocument()
+
+    expect(
+      screen.getByRole('heading', {name: /create product/i}),
+    ).toBeInTheDocument()
+  })
+})
